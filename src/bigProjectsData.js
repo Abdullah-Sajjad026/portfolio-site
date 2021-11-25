322a286c9dbe7c6814be87db804ce5de9b3e66d1
Abdullah Sajjad <nogame>
2021-11-25 21:14:14 +0500

    updated images

--- src/bigProjectsData.js
+++ src/bigProjectsData.js
@@ -2,21 +2,21 @@ const bigProjectsData = [
     {
         title: 'ResourcesMark',
         description: `A curated list of resources for Learners and Front-End Developers.`,
-        img: 'resourcesmark-ss.png',
+        img: 'resourcesmark.png',
         link: 'https://resourcesmark.netlify.app'
     },
     {
         title: 'The Apps Land',
         description: `I developed this website for "TheAppsLand", a software-development and publishing
                         based software house located in RahimYarKhan, Pakistan.`,
-        img: 'theappsland-ss.png',
+        img: 'theappsland.png',
         link: 'https://theappsland.netlify.app',
     },
 
     {
         title: 'Natours',
         description: `Website for a fictional Tour Organizing Company`,
-        img: 'natours-ss.png',
+        img: 'natours.png',
         link: 'https://natours-a.netlify.app',
     },
     {
@@ -34,25 +34,25 @@ const bigProjectsData = [
     {
         title: 'Newsgriddy',
         description: ` Self-coded Website. Design inspiraton from Brad Traversy's Newsgrid website.`,
-        img: 'newsgriddy-ss.png',
+        img: 'newsgriddy.png',
         link: 'https://newsgriddy.netlify.app/',
     },
     {
         title: 'Trillotel',
         description: ` Website for a fictional hotel booking Company.`,
-        img: 'trillotel-ss.png',
+        img: 'trillotel.png',
         link: 'http://trillotel.netlify.app/',
     },
     {
         title: 'Nexterrhouse',
         description: `Website for a fictional House Selling company.`,
-        img: 'nexter-ss.png',
+        img: 'nexter.png',
         link: 'https://nexterrhouse.netlify.app/',
     },
     {
         title: 'OmniFood',
         description: `Website for a fictional food delivery hotel.`,
-        img: 'omnifood-ss.png',
+        img: 'omnifood.png',
         link: 'https://omnifood-orpin.vercel.app/',
     },
 ]
