24b55a78ecac0de099353259887a721b4cae388d
Abdullah Sajjad <70557122+Abdullah-Sajjad026@users.noreply.github.com>
2022-01-31 15:05:58 +0500

    Updated footer github link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,11 +13,11 @@ const Footer = () => {
                     src="images/twitter.svg" alt="Social Logo" className="footer__logo" /></a>
                 <a href="https://codepen.io/abdullah-webdev" rel="noreferrer" target="_blank" className="footer__link"><img
                     src="images/codepen.svg" alt="Social Logo" className="footer__logo" /></a>
-                <a href="https://www.github.com/Abdullah-webdev/" rel="noreferrer" target="_blank" className="footer__link"><img
+                <a href="https://www.github.com/Abdullah-Sajjad026/" rel="noreferrer" target="_blank" className="footer__link"><img
                     src="images/github.svg" alt="Social Logo" className="footer__logo" /></a>
                 <a href="https://www.frontendmentor.io/profile/Abdullah-webdev" rel="noreferrer" target="_blank" className="footer__link"><img
                     src="images/frontendmentor.jpeg" alt="Social Logo" className="footer__logo" /></a>
-                <a href="mailto:abdullahwebdev@gmail.com" rel="noreferrer" target="_blank" className="footer__link"><img src="images/gmail.svg"
+                <a href="mailto:abdullahsa026@gmail.com" rel="noreferrer" target="_blank" className="footer__link"><img src="images/gmail.svg"
                     alt="Social Logo" className="footer__logo" /></a>
             </div>
             <p className="copyright-text">&copy; Copyright by Abdullah Sajjad. All Rights reserved. <br /> Designed and developed
