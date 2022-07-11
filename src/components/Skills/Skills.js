c9b5755664d3a24a406555ce273b39dc2a316feb
Abdullah Sajjad <abdullah@letshirepro.com>
2022-07-11 23:02:07 +0500

    added nextjs

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,27 +1,26 @@
-import SkillCard from '../SkillCard/SkillCard'
+import SkillCard from "../SkillCard/SkillCard";
 
 const Skills = () => {
-    return (
-        <section id="skills" className='skills'>
+  return (
+    <section id="skills" className="skills">
+      <div className="wrapper">
+        <div className="title-box">
+          <h2 className="heading-2">My Skills</h2>
+        </div>
 
-            <div className="wrapper">
+        <div className="skills__content" data-aos="zoom-out-up">
+          <SkillCard img="html5.svg" skillName="HTML5" />
+          <SkillCard img="css3.svg" skillName="CSS3" />
+          <SkillCard img="sass.svg" skillName="SASS/SCSS" />
+          <SkillCard img="bootstrap.svg" skillName="Bootstrap" />
+          <SkillCard img="js.svg" skillName="JavaScript" />
+          <SkillCard img="react.svg" skillName="ReactJS" />
+          <SkillCard img="nextjs2.svg" skillName="NextJS" />
+          <SkillCard img="git.svg" skillName="Git/Github" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-                <div className="title-box">
-                    <h2 className="heading-2">My Skills</h2>
-                </div>
-
-                <div className="skills__content" data-aos="zoom-out-up">
-                    <SkillCard img='html5.svg' skillName='HTML5' />
-                    <SkillCard img='css3.svg' skillName='CSS3' />
-                    <SkillCard img='sass.svg' skillName='SASS/SCSS' />
-                    <SkillCard img='bootstrap.svg' skillName='Bootstrap' />
-                    <SkillCard img='js.svg' skillName='JavaScript' />
-                    <SkillCard img='react.svg' skillName='ReactJS' />
-                    <SkillCard img='git.svg' skillName='Git/Github' />
-                </div>
-            </div>
-        </section>
-    )
-}
-
-export default Skills
+export default Skills;
