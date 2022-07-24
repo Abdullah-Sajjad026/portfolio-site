88035dbed1ba11780c080833e723a77f9afff18f
Abdullah Sajjad <abdullah@letshirepro.com>
2022-07-24 15:45:33 +0500

    added linkedin links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,31 +1,104 @@
 const Footer = () => {
-    return (
-        <footer className="footer" id="footer">
+  return (
+    <footer className="footer" id="footer">
+      <h3 className="heading-3">Living, learning, & leveling up everyday.</h3>
 
-            <h3 className="heading-3">Living, learning, & leveling up everyday.</h3>
+      <div className="footer__content">
+        <a
+          href="https://www.linkedin.com/in/abdullahsajjad026"
+          rel="noreferrer"
+          target="_blank"
+          className="footer__link"
+        >
+          <img
+            src="images/linkedin.png"
+            alt="Social Logo"
+            className="footer__logo"
+          />
+        </a>
+        <a
+          href="https://www.facebook.com/profile.php?id=100047766791619"
+          rel="noreferrer"
+          target="_blank"
+          className="footer__link"
+        >
+          <img src="images/fb.svg" alt="Social Logo" className="footer__logo" />
+        </a>
+        <a
+          href="https://www.instagram.com/ch_abdullah_sajjad/"
+          rel="noreferrer"
+          target="_blank"
+          className="footer__link"
+        >
+          <img src="images/ig.svg" alt="Social Logo" className="footer__logo" />
+        </a>
+        <a
+          href="https://twitter.com/Abdullah_webdev"
+          rel="noreferrer"
+          target="_blank"
+          className="footer__link"
+        >
+          <img
+            src="images/twitter.svg"
+            alt="Social Logo"
+            className="footer__logo"
+          />
+        </a>
+        <a
+          href="https://codepen.io/abdullah-webdev"
+          rel="noreferrer"
+          target="_blank"
+          className="footer__link"
+        >
+          <img
+            src="images/codepen.svg"
+            alt="Social Logo"
+            className="footer__logo"
+          />
+        </a>
+        <a
+          href="https://www.github.com/Abdullah-Sajjad026/"
+          rel="noreferrer"
+          target="_blank"
+          className="footer__link"
+        >
+          <img
+            src="images/github.svg"
+            alt="Social Logo"
+            className="footer__logo"
+          />
+        </a>
+        <a
+          href="https://www.frontendmentor.io/profile/Abdullah-webdev"
+          rel="noreferrer"
+          target="_blank"
+          className="footer__link"
+        >
+          <img
+            src="images/frontendmentor.jpeg"
+            alt="Social Logo"
+            className="footer__logo"
+          />
+        </a>
+        <a
+          href="mailto:abdullahsa026@gmail.com"
+          rel="noreferrer"
+          target="_blank"
+          className="footer__link"
+        >
+          <img
+            src="images/gmail.svg"
+            alt="Social Logo"
+            className="footer__logo"
+          />
+        </a>
+      </div>
+      <p className="copyright-text">
+        &copy; Copyright by Abdullah Sajjad. All Rights reserved. <br />{" "}
+        Designed and developed with ♥ by me.
+      </p>
+    </footer>
+  );
+};
 
-            <div className="footer__content">
-                <a href="https://www.facebook.com/profile.php?id=100047766791619" rel="noreferrer" target="_blank" className="footer__link"><img
-                    src="images/fb.svg" alt="Social Logo" className="footer__logo" /></a>
-                <a href="https://www.instagram.com/ch_abdullah_sajjad/" rel="noreferrer" target="_blank" className="footer__link"><img
-                    src="images/ig.svg" alt="Social Logo" className="footer__logo" /></a>
-                <a href="https://twitter.com/Abdullah_webdev" rel="noreferrer" target="_blank" className="footer__link"><img
-                    src="images/twitter.svg" alt="Social Logo" className="footer__logo" /></a>
-                <a href="https://codepen.io/abdullah-webdev" rel="noreferrer" target="_blank" className="footer__link"><img
-                    src="images/codepen.svg" alt="Social Logo" className="footer__logo" /></a>
-                <a href="https://www.github.com/Abdullah-Sajjad026/" rel="noreferrer" target="_blank" className="footer__link"><img
-                    src="images/github.svg" alt="Social Logo" className="footer__logo" /></a>
-                <a href="https://www.frontendmentor.io/profile/Abdullah-webdev" rel="noreferrer" target="_blank" className="footer__link"><img
-                    src="images/frontendmentor.jpeg" alt="Social Logo" className="footer__logo" /></a>
-                <a href="mailto:abdullahsa026@gmail.com" rel="noreferrer" target="_blank" className="footer__link"><img src="images/gmail.svg"
-                    alt="Social Logo" className="footer__logo" /></a>
-            </div>
-            <p className="copyright-text">&copy; Copyright by Abdullah Sajjad. All Rights reserved. <br /> Designed and developed
-                with ♥ by me.</p>
-
-
-        </footer>
-    )
-}
-
-export default Footer
+export default Footer;
