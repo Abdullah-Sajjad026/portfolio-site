573094ae73ff3f6741a5791aa61fa4da034682aa
Abdullah Sajjad <abdullah@letshirepro.com>
2022-07-24 17:00:41 +0500

    added vertixsoft

--- src/bigProjectsData.js
+++ src/bigProjectsData.js
@@ -1,75 +1,82 @@
 const bigProjectsData = [
-    {
-        title: 'Zomcore.',
-        description: `Zomcore is an ecommerce platform where you can buy premium quality clothes, electrical appliances and much much more.`,
-        img: 'zomcore.png',
-        link: 'https://zomcore.vercel.app/'
-    }, {
-        title: 'LOOPS',
-        description: `LOOPS is a software house in RahimYarKhan, Pakistan. This website was developed for LOOPS.`,
-        img: 'loops.png',
-        link: 'http://loops-tech.com/'
-    },
-    {
-        title: 'ResourcesMark',
-        description: `This website was developed for Front-end Developers where they can find a collection of useful resources for their next project.`,
-        img: 'resourcesmark.png',
-        link: 'https://resourcesmark.netlify.app'
-    },
-    {
-        title: 'The Apps Land',
-        description: `This website was developed for "TheAppsLand", a software-development and publishing
+  {
+    title: "Zomcore.",
+    description: `Zomcore is an ecommerce platform where you can buy premium quality clothes, electrical appliances and much much more.`,
+    img: "zomcore.png",
+    link: "https://zomcore.vercel.app/",
+  },
+  {
+    title: "The Apps Land",
+    description: `This website was developed for "TheAppsLand", a software-development and publishing
                         based software house located in RahimYarKhan, Pakistan.`,
-        img: 'theappsland.png',
-        link: 'https://theappsland.netlify.app',
-    },
+    img: "theappsland.png",
+    link: "https://theappsland.netlify.app",
+  },
+  {
+    title: "LOOPS",
+    description: `LOOPS is a software house in RahimYarKhan, Pakistan. This website was developed for LOOPS.`,
+    img: "loops.png",
+    link: "http://loops-tech.com/",
+  },
+  {
+    title: "ResourcesMark",
+    description: `This website was developed for Front-end Developers where they can find a collection of useful resources for their next project.`,
+    img: "resourcesmark.png",
+    link: "https://resourcesmark.netlify.app",
+  },
 
-    {
-        title: 'Natours',
-        description: `This website was built for a tour organizing company which helped them to promote their business.`,
-        img: 'natours.png',
-        link: 'https://natours-a.netlify.app',
-    },
-    {
-        title: 'Tasty Food Hotel',
-        description: `This website was built for a restaurant which helped their customers to geit in touch with them.`,
-        img: 'food.png',
-        link: 'https://food-mu.vercel.app',
-    },
-    {
-        title: 'Clipboardsite',
-        description: `This website was developed for a startup where all features of their product are listed.`,
-        img: 'clipboardsite.png',
-        link: 'https://clipboardsite.vercel.app',
-    },
-    {
-        title: 'Newsgriddy',
-        description: `This website template was developed for a news publishing organization.`,
-        img: 'newsgriddy.png',
-        link: 'https://newsgriddy.netlify.app/',
-    },
-    {
-        title: 'Trillotel',
-        description: `This website was developed for a hotel.`,
-        img: 'trillotel.png',
-        link: 'http://trillotel.netlify.app/',
-    },
-    {
-        title: 'Nexterrhouse',
-        description: `This website template was developed for a House Selling Company 'Nexter'.`,
-        img: 'nexter.png',
-        link: 'https://nexterrhouse.netlify.app/',
-    },
-    {
-        title: 'OmniFood',
-        description: `A website developed for a restaurant.`,
-        img: 'omnifood.png',
-        link: 'https://omnifood-orpin.vercel.app/',
-    },
-]
+  {
+    title: "Vertix Soft",
+    description: `The website developed for a software development company according to their needs.`,
+    img: "vertixsoft.png",
+    link: "https://vertixsoft.vercel.app",
+  },
 
-export default bigProjectsData
+  {
+    title: "Natours",
+    description: `This website was built for a tour organizing company which helped them to promote their business.`,
+    img: "natours.png",
+    link: "https://natours-a.netlify.app",
+  },
+  {
+    title: "Tasty Food Hotel",
+    description: `This website was built for a restaurant which helped their customers to geit in touch with them.`,
+    img: "food.png",
+    link: "https://food-mu.vercel.app",
+  },
+  {
+    title: "Clipboardsite",
+    description: `This website was developed for a startup where all features of their product are listed.`,
+    img: "clipboardsite.png",
+    link: "https://clipboardsite.vercel.app",
+  },
+  {
+    title: "Newsgriddy",
+    description: `This website template was developed for a news publishing organization.`,
+    img: "newsgriddy.png",
+    link: "https://newsgriddy.netlify.app/",
+  },
+  {
+    title: "Trillotel",
+    description: `This website was developed for a hotel.`,
+    img: "trillotel.png",
+    link: "http://trillotel.netlify.app/",
+  },
+  {
+    title: "Nexterrhouse",
+    description: `This website template was developed for a House Selling Company 'Nexter'.`,
+    img: "nexter.png",
+    link: "https://nexterrhouse.netlify.app/",
+  },
+  {
+    title: "OmniFood",
+    description: `A website developed for a restaurant.`,
+    img: "omnifood.png",
+    link: "https://omnifood-orpin.vercel.app/",
+  },
+];
 
+export default bigProjectsData;
 
 /*
 
